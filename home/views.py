25e6d14004afe1ececa8c747ffc4bdcf1a3a4b0c
Pranamya Kulal <spacedust26@gmail.com>
2025-04-01 22:35:29 +0530

    dashboard sidebar and layout

--- home/views.py
+++ home/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def dashboard(request):
+    return render(request, 'home/dashboard.html')
+
+def view_space(request):
+    return render(request, 'home/view_space.html')
+
+def view_equipment(request):
+    return render(request, 'home/view_equipment.html')
+
+def booking_cart(request):
+    return render(request, 'home/booking_cart.html')
+
+def history(request):
+    return render(request, 'home/history.html')
